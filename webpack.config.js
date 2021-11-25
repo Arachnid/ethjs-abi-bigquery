9f2ff761bc8d2e9aa87968ec02935b4a9a727275
Nick Johnson <arachnid@notdot.net>
2021-11-26 10:07:42 +1300

    Updated to ethers-abi v5

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,10 @@
 var path = require('path');
 
 module.exports = {
-  entry: './node_modules/ethjs-abi/src/index.js',
+  entry: './node_modules/@ethersproject/abi/lib/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'ethjs-abi.js',
+    filename: 'ethers-abi.js',
     library: 'abi',
     libraryTarget: 'var',
   },
